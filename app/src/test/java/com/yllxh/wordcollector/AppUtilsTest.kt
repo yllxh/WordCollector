d385b33bd547ac5ca96a7d4df57f837497391362
yllxh <yllxhd@gmail.com>
2019-10-27 02:13:55 -1000

    Added some tests for the AppUtils isValidWord() function.

--- app/src/test/java/com/yllxh/wordcollector/AppUtilsTest.kt
+++ app/src/test/java/com/yllxh/wordcollector/AppUtilsTest.kt
@@ -3,27 +3,36 @@ package com.yllxh.wordcollector
 import com.yllxh.wordcollector.AppUtils.Companion.isValidWord
 import com.yllxh.wordcollector.data.Word
 import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Test
 
 class AppUtilsTest{
     private lateinit var wordWithEmptyTerm: Word
     private lateinit var wordWithEmptyDefinition: Word
 
+    @Before
+    fun initWords(){
+        wordWithEmptyDefinition = Word("word", "")
+        wordWithEmptyTerm = Word("","definition")
+    }
 
     @Test
     fun wordIsValidWhenWordHasEmptyDefinitionOnly(){
-        wordWithEmptyDefinition = Word("word", "")
         assertTrue(isValidWord(wordWithEmptyDefinition))
     }
 
     @Test
     fun wordIsValidWhenWordHasEmptyTermOnly(){
-        wordWithEmptyTerm = Word("","definition")
         assertTrue(isValidWord(wordWithEmptyTerm))
     }
 
     @Test
     fun returnsFalseWhenWordIsEmpty(){
         assertFalse(isValidWord(Word()))
+    }
+
+    @Test
+    fun returnsTrueWhenComparingWordsWithDifferentContent(){
+        assertTrue(isValidWord(wordWithEmptyTerm, wordWithEmptyDefinition))
     }
 }
