1bee83215d24d23b617db6efd9420c6eeed8d105
yllxh <yllxhd@gmail.com>
2019-12-12 13:20:33 +0100

    Added Log statements to AppPreferences.

--- app/src/main/java/com/yllxh/wordcollector/utils/AppPreferences.kt
+++ app/src/main/java/com/yllxh/wordcollector/utils/AppPreferences.kt
@@ -1,12 +1,14 @@
 package com.yllxh.wordcollector.utils
 
 import android.content.Context
+import android.util.Log
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.preference.PreferenceManager
 import com.yllxh.wordcollector.R
 import com.yllxh.wordcollector.data.DEFAULT_CATEGORY_NAME
 
 
+private const val TAG: String = "AppPreference"
 private const val DAY_NIGHT_KEY = "dayNightKey"
 private const val LAST_SELECTED_CATEGORY = "lastSelectedCategory"
 
@@ -65,7 +67,7 @@ fun getLastSelectedCategory(context: Context): String {
 
     PreferenceManager.getDefaultSharedPreferences(context).apply {
 
-        return if (contains(LAST_SELECTED_CATEGORY)) {
+        val name = if (contains(LAST_SELECTED_CATEGORY)) {
             getString(LAST_SELECTED_CATEGORY, DEFAULT_CATEGORY_NAME) ?: DEFAULT_CATEGORY_NAME
         } else {
             edit().apply {
@@ -74,10 +76,13 @@ fun getLastSelectedCategory(context: Context): String {
             }
             DEFAULT_CATEGORY_NAME
         }
+        Log.d(TAG, "returning selected category: $name")
+        return name
     }
 }
 
 fun setLastSelectedCategory(context: Context, name: String) {
+    Log.d(TAG, "saving selected category: $name")
     PreferenceManager.getDefaultSharedPreferences(context).apply {
         edit().apply {
             putString(LAST_SELECTED_CATEGORY, name)
